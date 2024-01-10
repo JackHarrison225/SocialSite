4ba0f675700a19f1b87eda29ad26c5b2aefba30e
pedraamchamsaz <151649370+pedraamchamsaz@users.noreply.github.com>
2024-01-10 14:10:37 +0000

    Update pfp.jsx

--- socialapp/app/componants/pfp.jsx
+++ socialapp/app/componants/pfp.jsx
@@ -6,9 +6,8 @@ const ProfilePic = ({
      className2
 }) => {
   return (
-     <div className={className1}> {/* absolute float-left w-20 h-20 right-4 top-4 bg-[#000000] rounded-full overflow-x-hidden overflow-y-hidden for navbar*/}
-      {/*min-w-[150px] min-h-[150px] ml-[-30px] mt-[-30px] for navbar */}
-      <img className={className2}  
+     <div className=float-left w-20 h-20 right-4 top-4 bg-[#000000] rounded-full overflow-x-hidden overflow-y-hidden for navbar'>
+      <img className='min-w-[150px] min-h-[150px] ml-[-30px] mt-[-30px]}' 
       src={imageLink} alt="PFP" 
       />
     </div>
