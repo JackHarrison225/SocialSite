e8bcf17d66d9d532ddf60b5d9aa710b21a93b8ee
JackHarrison225 <Jh.rrison225@gmail.com>
2024-01-10 09:44:45 +0000

    pages and files made

--- socialapp/app/componants/AuthBox.jsx
+++ socialapp/app/componants/AuthBox.jsx
@@ -5,7 +5,7 @@ import Link from 'next/link'
 const AuthBox = () => {
   return (
     <div>
-      <h3 className='text-center text-3xl'>Sign In To Tick Talk</h3>
+      <h3 className='text-center text-3xl'>Sign In To Dino Book</h3>
       <form className='bg-[#71df99] w-96 h-52 rounded-xl flex flex-col px-5 py-5'>
           <label htmlFor="" className='ml-[2%]'>Username:</label>
           <input name='Username' className='bg-[#b9d9f8] rounded-full text-center placeholder-[#808080]' placeholder='JohnDoe@email.com'/>
