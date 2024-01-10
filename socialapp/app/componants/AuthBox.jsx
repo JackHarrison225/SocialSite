61212e1b2fc428aab74852960ac17f92a06ee89a
JackHarrison225 <Jh.rrison225@gmail.com>
2024-01-10 09:43:58 +0000

    pages and files made

--- socialapp/app/componants/AuthBox.jsx
+++ socialapp/app/componants/AuthBox.jsx
@@ -4,17 +4,21 @@ import Link from 'next/link'
 
 const AuthBox = () => {
   return (
-     <form className='bg-blue-300 w-96 h-52 rounded-xl flex flex-col px-5 py-5'>
-               <label htmlFor="" className='ml-[2%]'>Username:</label>
-               <input name='Username' className='bg-white rounded-full text-center' placeholder='JohnDoe@email.com'/>
-               <label htmlFor="Password" className='ml-[2%]'>Password:</label>
-               <input name='Password' type='password' className='bg-white rounded-full text-center' placeholder='********'/>
-               <a className='ml-[2%]'>Forgot Username or password?</a>
-               <div className='flex gap-5 justify-center pt-[10px]'>
-                    <button className='bg-white py-1 px-2 rounded-full'>Sign Up</button>
-                    <button className='bg-white py-1 px-2 rounded-full'>Sign In</button>
-               </div>     
-     </form>
+    <div>
+      <h3 className='text-center text-3xl'>Sign In To Tick Talk</h3>
+      <form className='bg-[#71df99] w-96 h-52 rounded-xl flex flex-col px-5 py-5'>
+          <label htmlFor="" className='ml-[2%]'>Username:</label>
+          <input name='Username' className='bg-[#b9d9f8] rounded-full text-center placeholder-[#808080]' placeholder='JohnDoe@email.com'/>
+          <label htmlFor="Password" className='ml-[2%]'>Password:</label>
+          <input name='Password' type='password' className='bg-[#b9d9f8] rounded-full text-center placeholder-[#808080]' placeholder='********'/>
+          <a className='ml-[2%]'>Forgot Username or password?</a>
+          <div className='flex gap-5 justify-center pt-[10px]'>
+            <button className='bg-[#4CAF50] py-1 px-2 rounded-full'>Sign Up</button>
+            <button className='bg-[#4CAF50] py-1 px-2 rounded-full'>Sign In</button>
+          </div>     
+      </form>
+    </div>
+
   )
 }
 
