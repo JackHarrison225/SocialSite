ebfae475a7251489ab025b86f64bdf5c70aee6dd
JackHarrison225 <Jh.rrison225@gmail.com>
2024-01-10 22:56:59 +0000

    login and out funtional to a point as well as user creation

--- socialapp/app/componants/Logo.jsx
+++ socialapp/app/componants/Logo.jsx
@@ -4,8 +4,9 @@ import React from 'react'
 function Logo() {
   return (
     <div className='absolute float-left w-20 h-20 left-4 top-4 bg-[#71df99] rounded-xl overflow-x-hidden overflow-y-hidden'>
-               <img className='min-w-[150px] min-h-[150px] ml-[-30px] mt-[-30px]' 
-               src="https://i.postimg.cc/26tYPKrp/ebb935df-9554-4c11-89ca-ebc0aa559211.png" alt="DinoLogo" />
+		<img className='min-w-[150px] min-h-[150px] ml-[-30px] mt-[-30px]' 
+		src="https://i.postimg.cc/26tYPKrp/ebb935df-9554-4c11-89ca-ebc0aa559211.png" 
+		alt="DinoLogo" />
     </div>
   )
 }
