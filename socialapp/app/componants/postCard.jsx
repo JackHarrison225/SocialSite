c08de8d520f7c871507faafcbec8f1aff4f6ab4f
JackHarrison225 <Jh.rrison225@gmail.com>
2024-01-12 15:13:41 +0000

    mostly sorted

--- socialapp/app/componants/postCard.jsx
+++ socialapp/app/componants/postCard.jsx
@@ -7,18 +7,18 @@ const PostCard = ({
     user
 }) => {
     return (
-    <div>
-     <div className='w-1/4 h-96 rounded-lg overflow-hidden relative'>
-        <img src={image} alt={text} className='w-full h-full object-cover'/>
-        {liked ? <ThumbsUp
-            className='absolute bottom-2 right-2 text-green-600'
-            size={24}
-        /> : <ThumbsUp
-            className='absolute bottom-2 right-2 text-white'
-            size={24}
-        /> }
-    </div>
-    </div>
+    	<div>
+		<div className='w-1/4 h-96 rounded-lg overflow-hidden relative'>
+			<img src={image} alt={text} className='w-full h-full object-cover'/>
+			{liked ? <ThumbsUp
+				className='absolute bottom-2 right-2 text-green-600'
+				size={24}
+			/> : <ThumbsUp
+				className='absolute bottom-2 right-2 text-white'
+				size={24}
+			/> }
+		</div>
+    	</div>
   )
 }
 export default PostCard
--- socialapp/app/homePage/page.js
+++ socialapp/app/homePage/page.js
@@ -1,33 +1,33 @@
 "use client"
 import React, { useEffect, useState } from 'react';
 import NavBar from '../componants/NavBar';
-import PostCard from '../componants/postCard'
+import PostCard from '../componants/postCard.jsx'
 const Page = () => {
-  const [posts, setPosts] = useState([]);
-  useEffect(() => {
-    const storedPosts = localStorage.getItem('globalPosts');
-    if (storedPosts) {
-      const parsedPosts = JSON.parse(storedPosts);
-      setPosts(parsedPosts);
-    }
-  }, []);
-  return (
-    <div>
-      <NavBar />
-      <div className='p-4'>
-        <div className='flex flex-wrap gap-20 mt-28 justify-center'>
-          {posts.map((post) => (
-            <PostCard
-              key={post.post.id}
-              text={post.post.title}
-              image={post.post.image}
-              liked={post.liked}
-              user={post.user}
-            />
-            ))}
-        </div>
-      </div>
-    </div>
-  );
+	const [posts, setPosts] = useState([]);
+	useEffect(() => {
+		const storedPosts = localStorage.getItem('globalPosts');
+		if (storedPosts) {
+			const parsedPosts = JSON.parse(storedPosts);
+			setPosts(parsedPosts);
+		}
+	}, []);
+	return (
+		<div>
+			<NavBar />
+			<div className='p-4'>
+				<div className='flex flex-wrap gap-20 mt-28 justify-center'>
+					{posts.map((post) => (
+						<PostCard
+							key={post.post.id}
+							text={post.post.title}
+							image={post.post.image}
+							liked={post.liked}
+							user={post.user}
+						/>
+						))}
+				</div>
+			</div>
+		</div>
+	);
 };
 export default Page;
