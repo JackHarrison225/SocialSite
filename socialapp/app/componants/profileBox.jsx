64ce8c6f1aec7075a5b74735245a018f8da043d9
Jack Harrison <151574723+JackHarrison225@users.noreply.github.com>
2024-01-12 15:25:20 +0000

    Update profileBox.jsx

--- socialapp/app/componants/profileBox.jsx
+++ socialapp/app/componants/profileBox.jsx
@@ -1,7 +1,7 @@
 'use client'
 import React, { useEffect, useState } from 'react'
 import ProfilePic from './pfp'
-import PostCard from './postCard'
+import PostCard from './PostCard.jsx'
 
 const ProfileBox = (
 ) => {
